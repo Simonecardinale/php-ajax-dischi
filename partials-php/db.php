364a83cb6bd1d396c7ee3b4a1f0638c21768a475
Simone Cardinale <simonecardinale8@gmail.com>
2021-03-01 15:14:55 +0100

    db created

---
+++ partials-php/db.php
@@ -0,0 +1,57 @@
+<?php 
+
+    $dischiArray = [
+        [
+            "titolo" => "Replace You",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Let it Go",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "As A Butterfly",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Angels of Clarity",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "You Should Know",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Warrior",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Two Faced",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Erased",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Dreaming",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+        [
+            "titolo" => "Bulletproof",
+            "autore" => "Dead by April",
+            "genere" => "Metal"
+        ],
+    ];
+
+
+?>
